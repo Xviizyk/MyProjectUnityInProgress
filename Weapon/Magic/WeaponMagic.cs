e506d5bda5e637542857c538653b000e5d453f41
Xviizyk <artursarture@gmail.com>
2025-11-17 10:12:12 +0300

    IDK

---
+++ Weapon/Magic/WeaponMagic.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class WeaponMagic : MonoBehaviour
+{
+    [Header("Префабы оружия")]
+    public Weapon w;
+    [SerializeField] private GameObject WeaponObject;
+    [SerializeField] private Transform muzzlePoint;
+
+    [Header("Конфигурация")]
+    [SerializeField] private ulong PlayerSpeedmultiplier;
+    [SerializeField] private ulong Damage;
+    [SerializeField] private ulong SplashDamage;
+    [SerializeField] private ulong SplashRange;
+    [SerializeField] private ushort MaxAmmo;
+    [SerializeField] private float ReloadTime;
+    [SerializeField] private float BetweenFireTime;
+    [SerializeField] private float ManaUse;
+
+    [Header("Настройка патронов")]
+    [SerializeField] private GameObject WeaponAmmoObject; 
+    [SerializeField] private string WeaponAmmoName;
+
+    public void privateUpdate(){
+        
+    }
+}
